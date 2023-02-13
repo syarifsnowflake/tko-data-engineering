fa7158df33f4c62fd18f51b03bc13a85667b73ba
syarif hidayatullah <syarif.hidayatullah@snowflake.com>
2023-02-13 17:59:45 +0700

    updated fahrenheit_to_celcius

--- steps/05_fahrenheit_to_celsius_udf/app.py
+++ steps/05_fahrenheit_to_celsius_udf/app.py
@@ -15,6 +15,8 @@ from scipy.constants import convert_temperature
 def main(temp_f: float) -> float:
     return convert_temperature(float(temp_f), 'F', 'C')
 
+# def main(temp_f: float) -> float:
+    # return (float(temp_f) - 32) * (5/9)
 
 # For local debugging
 # Be aware you may need to type-convert arguments if you add input parameters
